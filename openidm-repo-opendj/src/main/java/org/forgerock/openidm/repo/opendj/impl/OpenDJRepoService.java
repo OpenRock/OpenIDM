77a5a98c8430e0a1af6864d92862d69c0bfaad1b
Jim Mitchener <jim.mitchener@forgerock.com>
2015-04-15 15:38:25 +0000

    Update required dependencies and create support classes

--- openidm-repo-opendj/src/main/java/org/forgerock/openidm/repo/opendj/impl/OpenDJRepoService.java
+++ openidm-repo-opendj/src/main/java/org/forgerock/openidm/repo/opendj/impl/OpenDJRepoService.java
@@ -1,10 +1,11 @@
-package org.forgerock.openidm.repo.opendj;
+package org.forgerock.openidm.repo.opendj.impl;
 
 import org.forgerock.json.fluent.JsonValue;
 import org.forgerock.json.resource.CollectionResourceProvider;
 import org.forgerock.json.resource.Request;
 import org.forgerock.opendj.rest2ldap.Rest2LDAP;
 import org.forgerock.openidm.repo.crest.CRESTRepoService;
+import org.forgerock.util.time.TimeService;
 
 public class OpenDJRepoService extends CRESTRepoService {
     @Override
@@ -12,4 +13,9 @@ public class OpenDJRepoService extends CRESTRepoService {
         // TODO - calculate the resource id based on the request
         return null;
     }
+
+    /**
+     * Called when the bundle is stopped. Performs any necessary cleanup tasks.
+     */
+    void cleanup() { }
 }
