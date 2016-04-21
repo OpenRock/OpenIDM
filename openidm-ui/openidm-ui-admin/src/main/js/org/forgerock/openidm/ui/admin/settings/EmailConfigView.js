c0a79fb6dfc27d6979b8e0aaf124d8a1cecbc745
sean oneill <sean.oneill@forgerock.com>
2016-04-21 10:38:58 -0700

    OPENIDM-5632 added prevent default to stop default form submission

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/settings/EmailConfigView.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/settings/EmailConfigView.js
@@ -132,7 +132,8 @@ define("org/forgerock/openidm/ui/admin/settings/EmailConfigView", [
             this.data.password = $(e.currentTarget).val();
         },
 
-        save: function() {
+        save: function(e) {
+            e.preventDefault();
             var formData = form2js("emailConfigForm",".", true);
 
             _.extend(this.data.config, formData);
