70fef104550acecb65039252f942a1f21a0886bf
jason.browne <jason.browne@forgerock.com>
2016-05-09 08:14:12 -0700

    OPENIDM-5190 -Display target sample when using full source object in transform

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/mapping/properties/AttributesGridView.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/mapping/properties/AttributesGridView.js
@@ -352,7 +352,9 @@ define("org/forgerock/openidm/ui/admin/mapping/properties/AttributesGridView", [
                                 }
 
                                 if(!attributes.evalResult || !attributes.evalResult.conditionResults || attributes.evalResult.conditionResults.result) {
-                                    if (attributes.sample !== null) {
+                                    if (attributes.evalResult){
+                                        locals.textMuted = attributes.evalResult.transformResults;
+                                    } else if (attributes.sample !== null) {
                                         if (attributes.evalResult && attributes.evalResult.transformResults) {
                                             locals.textMuted = attributes.evalResult.transformResults;
                                         } else {
