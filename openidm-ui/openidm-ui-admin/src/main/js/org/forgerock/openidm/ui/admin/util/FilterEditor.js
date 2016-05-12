67c34b7bfcfb855fad3cc46f788ed86ccf987f4a
Javier Quiroz <javier.quiroz@forgerock.com>
2016-05-12 12:12:28 -0700

    OPENIDM-5716 -- Fixes filter builder not clearing down to one line when 'None of..' is selected.

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/util/FilterEditor.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/util/FilterEditor.js
@@ -60,32 +60,40 @@ define("org/forgerock/openidm/ui/admin/util/FilterEditor", [
 
                 return {current: node, parent: previousNode, path: objectPath};
             },
-            removeNode: function (e, callback) {
-                var context = this.getExpressionContext(e);
+
+            removeNode: function(e, callback) {
+                this.deleteNode(e);
+                this.renderExpressionTree(callback);
+            },
+
+            deleteNode: function(e, callback) {
+                var context = this.getExpressionContext(e),
+                    parent,
+                    children;
 
                 if (!_.isNull(context.parent)) {
-                    context.parent.children = _.reject(context.parent.children, function (c) { return c === context.current; });
-                    if (context.parent.children.length !== 0) {
-                        this.data.filterString = this.getFilterString();
+                    children = context.children;
+
+                    children = _.reject(children, function(c) { return c === context.current; });
+                    if (children.length !== 0) {
+                        this.setFilterString();
                     } else {
                         e.target = $(":input:first", $(e.target).parents(".node[index]")[1])[0];
                         return this.removeNode(e);
                     }
                 } else {
-                    this.data.filter = { "op": "none", "children": []};
-                    this.data.filterString = "";
+                    context.current.children.pop();
                 }
-
-                this.renderExpressionTree(callback);
             },
-            createNode: function (e) {
+
+            createNode: function(e) {
                 var context = this.getExpressionContext(e),
                     node = context.current;
                 if (!node.children) {
                     node.children = [];
                 }
                 node.children.push({name: "", value: "", tag: "equalityMatch", children: [], op: "expr"});
-                this.data.filterString = this.getFilterString();
+                this.setFilterString();
             },
 
             addNodeAndReRender: function(e, callback) {
@@ -93,36 +101,47 @@ define("org/forgerock/openidm/ui/admin/util/FilterEditor", [
                 this.renderExpressionTree(callback);
             },
 
-            updateNodeValue: function (e, callback) {
+            updateNodeValue: function(e, callback) {
                 var context = this.getExpressionContext(e),
                     node = context.current,
                     field = $(e.target),
+                    createNode = this.createNode.bind(this, e),
+                    deleteNode = this.deleteNode.bind(this, e),
                     redrawContainer = false;
 
+                // handle field types
                 if (field.hasClass("op")) {
                     redrawContainer = true;
                     node.op = field.val();
-                    if (node.op === "expr") {
+
+                    if (node.op === "none") {
+                        _.times(node.children.length, deleteNode);
+                    } else if (node.op === "expr") {
                         node.name = "";
                         node.value = "";
                         node.tag = "equalityMatch";
                         node.children = [];
-                    } else if (node.op === "none") {
-                        node.children = [];
-                    } else if (!node.children || !node.children.length) {
-                        // create 2 nodes for 'and'/'or' comparisons
-                        if (node.op === "and" || node.op === "or") {
+                    } else if (node.op === "not") {
+                        if (!node.children || !node.children.length) {
                             this.createNode(e);
-                            this.createNode(e);
-                        // create 1 node for other types of comparisons
                         } else {
+                            _.times(node.children.length, deleteNode);
                             this.createNode(e);
                         }
+                    } else if (node.op === "and" || node.op === "or") {
+                        if (node.children) {
+                            if (node.children.length < 2) {
+                                // add as many as it takes to get to 2 nodes
+                                _.times(2 - node.children.length, createNode);
+                            }
+                        } else {
+                            _.times(2, createNode);
+                        }
                     }
+                // end of op handlers
                 } else if (field.hasClass("name")) {
-
                     if (field.parent().siblings(".tag-body").find(".tag").val() === "extensibleMatchAND") {
-                        node.extensible.matchType=field.val();
+                        node.extensible.matchType = field.val();
                         node.name = field.val() + ":1.2.840.113556.1.4.803";
                     } else if (field.parent().siblings(".tag-body").find(".tag").val() === "extensibleMatchOR") {
                         node.extensible.matchType = field.val();
@@ -132,7 +151,7 @@ define("org/forgerock/openidm/ui/admin/util/FilterEditor", [
                             node.name = field.val();
                         }
                     }
-
+                // end of name handlers
                 } else if (field.hasClass("tag")) {
 
                     switch (field.val()) {
@@ -171,7 +190,7 @@ define("org/forgerock/openidm/ui/admin/util/FilterEditor", [
                             node.name = field.parent().siblings(".name-body").find(".name").val();
                             node.tag = field.val();
                     }
-
+                // end of tag handlers
                 } else if (field.hasClass("value")) {
                     if (field.parent().siblings(".tag-body").find(".tag").val().match(/^extensibleMatch/)) {
                         node.extensible.value = field.val();
@@ -179,11 +198,12 @@ define("org/forgerock/openidm/ui/admin/util/FilterEditor", [
 
                     node.value = field.val();
                 }
+                // end of field type control flow.
 
                 if (node && node.op !== "none") {
-                    this.data.filterString = this.getFilterString();
+                    this.setFilterString();
                 } else {
-                    this.data.filterString = "";
+                    this.setFilterString("");
                 }
 
                 if (redrawContainer) {
@@ -193,11 +213,19 @@ define("org/forgerock/openidm/ui/admin/util/FilterEditor", [
                 }
             },
 
-            renderExpressionTree: function (callback) {
-                if(callback) {
+            renderExpressionTree: function(callback) {
+                if (callback) {
                     uiUtils.renderTemplate(this.template, this.$el, _.extend({}, conf.globalData, this.data), callback, "replace");
                 } else {
                     uiUtils.renderTemplate(this.template, this.$el, _.extend({}, conf.globalData, this.data), $.noop(), "replace");
+                }
+            },
+
+            setFilterString: function(string) {
+                if (string) {
+                    this.data.filterString = string;
+                } else {
+                    this.data.filterString = this.getFilterString();
                 }
             }
         }),
